ad7571d4126bcb141478af7ee9f9659168d7c046
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2025-08-04 19:49:36 -0500

    Changes Files - New Project

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+</script>
+
+<main>
+  <header>
+    <h1>⚡ links-of-jct</h1>
+  </header>
+  <nav>
+    <strong>Mis links y entradas y proyectos que he creado y pues este es mi index</strong>
+    <br>
+    <strong>Pues obviamente los links y demas info abajo jajaja igual pues si es util eso creo xd</strong>
+  </nav>
+  <section>
+    <h2>💻 Mi Links</h2>
+    <div>
+      <p><strong>👨‍💻 portfolio</strong></p>
+      <p><cite>✨ A simple portfolio site crafted with care and code.</cite></p>
+      <code>👉 con astro.js y vite.js y tailwind.css</code>
+      <button><a href="https://jeanctech.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Web 🌐</a></button>
+      <br>
+      <button><a href="https://github.com/jeanctech/portfolio" target="_blank" rel="noopener noreferrer">Ver Codigo 👀</a></button>
+    </div>
+        <div>
+      <p><strong>👨‍💻 portfolio</strong></p>
+      <p><cite>✨ A simple portfolio site crafted with care and code.</cite></p>
+      <code>👉 con astro.js y vite.js y tailwind.css</code>
+      <button><a href="https://jeanctech.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Web 🌐</a></button>
+      <br>
+      <button><a href="https://github.com/jeanctech/portfolio" target="_blank" rel="noopener noreferrer">Ver Codigo 👀</a></button>
+    </div>    <div>
+      <p><strong>👨‍💻 portfolio</strong></p>
+      <p><cite>✨ A simple portfolio site crafted with care and code.</cite></p>
+      <code>👉 con astro.js y vite.js y tailwind.css</code>
+      <button><a href="https://jeanctech.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Web 🌐</a></button>
+      <br>
+      <button><a href="https://github.com/jeanctech/portfolio" target="_blank" rel="noopener noreferrer">Ver Codigo 👀</a></button>
+    </div>    <div>
+      <p><strong>👨‍💻 portfolio</strong></p>
+      <p><cite>✨ A simple portfolio site crafted with care and code.</cite></p>
+      <code>👉 con astro.js y vite.js y tailwind.css</code>
+      <button><a href="https://jeanctech.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Web 🌐</a></button>
+      <br>
+      <button><a href="https://github.com/jeanctech/portfolio" target="_blank" rel="noopener noreferrer">Ver Codigo 👀</a></button>
+    </div>    <div>
+      <p><strong>👨‍💻 portfolio</strong></p>
+      <p><cite>✨ A simple portfolio site crafted with care and code.</cite></p>
+      <code>👉 con astro.js y vite.js y tailwind.css</code>
+      <button><a href="https://jeanctech.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Web 🌐</a></button>
+      <br>
+      <button><a href="https://github.com/jeanctech/portfolio" target="_blank" rel="noopener noreferrer">Ver Codigo 👀</a></button>
+    </div>    <div>
+      <p><strong>👨‍💻 portfolio</strong></p>
+      <p><cite>✨ A simple portfolio site crafted with care and code.</cite></p>
+      <code>👉 con astro.js y vite.js y tailwind.css</code>
+      <button><a href="https://jeanctech.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Web 🌐</a></button>
+      <br>
+      <button><a href="https://github.com/jeanctech/portfolio" target="_blank" rel="noopener noreferrer">Ver Codigo 👀</a></button>
+    </div>
+  </section>
+</main>
+
+<style>
+</style>
